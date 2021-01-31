f38873dc1de9bd96a49124096bfcd805b566b813
unknown <ahsraful.freelancer@gmail.com>
2021-01-31 14:16:58 +0600

    add a about page

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,3 +1,4 @@
+// 
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
@@ -15,3 +16,23 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+//  album js
+$(document).ready(function() {
+	$('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title');
+			}
+		}
+	});
+});
